1e3226ab8de81a1648b5f5b9c1ee6d9a229d8e95
Drashti Nihalani <92103655+d-nihalani@users.noreply.github.com>
2026-06-25 23:15:35 +0530

    Update: top-k-elements-in-list - submission-2

--- Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // 1. Map to store the frequency vector as the key, 
-        // and a list (vector) of matching strings as the value.
-        map<vector<int>, vector<string>> mp;
-        
-        // 2. Build the map
-        for (int i = 0; i < strs.size(); i++) {
-            vector<int> temp(26, 0); // Create the character count vector
-            
-            // Count characters for the current string
-            for (char c : strs[i]) {
-                temp[c - 'a']++;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+         for (int n : nums) {
+            count[n]++;
+        }
+
+        vector<vector<int>> numbers(nums.size() + 1); 
+        // store all numbers that appear n times
+        for(const auto& [a,b] : count){
+            numbers[b].push_back(a);
+        }
+
+        vector<int> res;
+        for(int i= nums.size(); i>0; --i){
+            for(int n : numbers[i]){
+                res.push_back(n);
             }
-            
-            // Append the original string to the list for this specific count vector
-            mp[temp].push_back(strs[i]); 
-        }
-        
-        // 3. Extract the grouped anagrams into the final result
-        vector<vector<string>> result;
-        
-        // Correct way to iterate over a map in C++
-        for (auto pair : mp) {
-            result.push_back(pair.second); // pair.second is the vector<string>
-        }
-        
-        return result;
+                if(res.size()==k) break;
+        } return res;
     }
 };
