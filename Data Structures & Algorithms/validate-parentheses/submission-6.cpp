40ad771d1d6e6972caf4a15ef03d23008ee7af58
Drashti Nihalani <92103655+d-nihalani@users.noreply.github.com>
2026-07-06 15:04:50 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> st;
+        if (s.length() % 2 != 0 || s.length() == 0) return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '{' || s[i] == '[' || s[i] == '(')
+                st.push(s[i]);
+            else if (!st.empty() &&
+                     ((s[i] == ']' && st.top() == '[') || 
+                      (s[i] == '}' && st.top() == '{') ||
+                      (s[i] == ')' && st.top() == '(')))
+                st.pop();
+            else
+                return false;
+        }
+        if (st.empty()) return true;
+        return false;
+    }
+};
