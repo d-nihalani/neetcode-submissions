9f7591d1fa6acc3712484c9b407fce9578feb227
Drashti Nihalani <92103655+d-nihalani@users.noreply.github.com>
2026-07-08 00:04:34 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        stack<pair<int, int>> st;
+        vector<int> ans(temp.size(), 0);
+        for (int i = temp.size()-1 ; i >=0 ; i--) {
+            if (st.empty()) {
+                st.push({temp[i], i});
+                continue;
+            }
+
+            while (!st.empty() && temp[i] >= st.top().first) st.pop();
+
+            if ( !st.empty() && temp[i] < st.top().first) {
+                ans[i] = st.top().second - i;
+            }
+            st.push({temp[i], i});
+        }return ans;
+    }
+};
