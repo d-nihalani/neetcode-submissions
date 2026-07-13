3576c6caa55f888914bfd42f5d30fb08237dfaf4
Drashti Nihalani <92103655+d-nihalani@users.noreply.github.com>
2026-07-13 23:42:25 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,11 @@
+//topdown
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> v (n+1, -1);
+        for (int i=0; i<=n; i++){
+            if(i <=3 ) v[i] = i;
+            else v[i] = v[i-2] + v[i-1];
+        }return v[n];
+    }
+};
